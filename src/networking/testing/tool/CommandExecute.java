34fe45c4678b35d479a9c1fe0f507c865a7984ef
Namindu <namindu@live.com>
2023-11-15 09:38:22 +0530

    NNN

---
+++ src/networking/testing/tool/CommandExecute.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package networking.testing.tool;
+
+/**
+ *
+ * @author Namindu
+ */
+public class CommandExecute {
+    public static String execute(String command){
+        return "";
+    }
+}
